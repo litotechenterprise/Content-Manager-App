380c9da2b5b3dc8770aa3f12ac348c2382e70930
Pablo Endara-Santiago <litotechenterprise@gmail.com>
2023-12-27 16:01:33 -0500

    completed importing bulma and using basic template

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import "bulma/css/bulma.min.css";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
