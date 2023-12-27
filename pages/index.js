0db6b97bc21ffb400f223b4b3a339b14570d6ec9
Pablo Endara-Santiago <litotechenterprise@gmail.com>
2023-12-27 16:42:30 -0500

    completed creating API

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,25 @@ import Layout from "@/components/Layout";
 import NewsLetter from "@/components/Newsletter";
 import ResourceHighlight from "@/components/ResourceHighlight";
 import ResourceList from "@/components/ResourceList";
+import { resources } from "@/api/data";
 
-export default function Home() {
+function Home({ myData1, myData2 }) {
   return (
     <Layout>
-      <ResourceHighlight />
+      <ResourceHighlight resources={resources.slice(0, 2)} />
       <NewsLetter />
-      <ResourceList />
+      <ResourceList resources={resources.slice(2)} />
     </Layout>
   );
 }
+
+export function getStaticProps() {
+  return {
+    props: {
+      myData1: [1, 2, 3],
+      myData2: ["a", "b", "c"],
+    },
+  };
+}
+
+export default Home;
