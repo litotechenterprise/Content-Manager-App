42c11bad54118a1eb7a7f8369d8af657ad207bb6
Pablo Endara-Santiago <litotechenterprise@gmail.com>
2023-12-27 16:07:22 -0500

    added layout

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,14 @@
-import { Inter } from "next/font/google";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Layout from "@/components/Layout";
 import NewsLetter from "@/components/Newsletter";
 import ResourceHighlight from "@/components/ResourceHighlight";
 import ResourceList from "@/components/ResourceList";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <>
-      <Navbar />
+    <Layout>
       <ResourceHighlight />
       <NewsLetter />
       <ResourceList />
-      <Footer />
-    </>
+    </Layout>
   );
 }
